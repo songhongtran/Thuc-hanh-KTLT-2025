b82604047b3ba23ad78a82bf9892a64e6cfce177
songhongtran <songhong27072005@gmail.com>
2025-03-12 20:38:58 +0700

    Add files via upload

---
+++ Bai2/bai2.11.py
@@ -0,0 +1,10 @@
+print("Sinh Vien : Trần Võ Sóng Hồng")
+print("MSSV : 235752020710005")
+l = [1, 'python', 1, 10]
+k = ['cse', 2, 'guntur', 8]
+m = []
+m.append(l)
+m.append(k)
+print(m)
+d = {1: 1, 2: k, 'combine_list': m}
+print(d)
