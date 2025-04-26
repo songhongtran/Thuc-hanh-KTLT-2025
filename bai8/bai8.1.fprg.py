e006ca6f625425a4f1f5a0b2dfef40bf9e33f517
songhongtran <songhong27072005@gmail.com>
2025-04-26 09:24:30 +0700

    Add files via upload

---
+++ bai8/bai8.1.fprg.py
@@ -0,0 +1,22 @@
+print("Sinh vien : Trần Võ Sóng Hồng")
+print("MSSV : 235752020710005")
+import turtle
+
+window = turtle.Screen()
+window.bgcolor("lightgreen")
+
+painter = turtle.Turtle()
+painter.fillcolor('blue')
+painter.pencolor('blue')
+painter.pensize(3)
+
+def drawsq(t, s):
+    for i in range(4):
+        t.forward(s)
+        t.left(90)
+
+for i in range(1, 180):
+    painter.left(18)
+    drawsq(painter, 200)
+
+window.mainloop()
---
+++ bai8/bai8.3.fprg.py
@@ -0,0 +1,15 @@
+print("Sinh vien : Trần Võ Sóng Hồng")
+print("MSSV : 235752020710005")
+import turtle
+colors = ["red", "green", "blue"]
+painter = turtle.Turtle()
+painter.pensize(3)
+for i in range(12):
+    color = colors[i % len(colors)] 
+    painter.pencolor(color)
+    painter.circle(100)
+    painter.right(30)
+    painter.left(60)
+    painter.setposition(0, 0)
+painter.hideturtle()
+turtle.done()
